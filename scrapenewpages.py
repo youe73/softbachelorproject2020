d2302f34008a0acecc0b0014e8a1b02a10eae9c3
Yosuke Ueda <yosukeueda@hotmail.com>
2020-09-11 13:14:04 +0200

    modified scrapenewpages.py

--- scrapenewpages.py
+++ scrapenewpages.py
@@ -2,11 +2,17 @@ import schedule
 import time 
 from scraper.scraperclass import Datascraper
 import time
+import sys
 
-url = "https://www.jobindex.dk/"
-newurl = "https://www.jobindex.dk/jobsoegning?jobage=14"
+incomingurl = sys.argv[1]
+searching = sys.argv[2]
+
+url = incomingurl
+newurl =url + searching
+
 crw = Datascraper()
 
+f = open("example_newlinks.txt","a",encoding="utf-8")
 newlinks = []
 days = newurl.split('=')[-1]
 linkpage = crw.requesting_url(newurl)
@@ -16,12 +22,11 @@ if not maxpages:
     maxpage=1
 else:
     maxpage = maxpages[-1].getText()
-    print("maxpage ",maxpage) 
-    #newlinks = []
+    print("maxpage ",maxpage)     
     count = 0
     for trip in range(int(maxpage)):
         trip+=1
-        scr_url = url + "&page=" + str(trip)
+        scr_url = url + "jobsoegning?page=" + str(trip) + "&jobage=14"
         print(scr_url)
         resp = crw.requesting_url(scr_url)             
         soup = crw.get_soup(resp)
@@ -32,14 +37,16 @@ else:
             for txts in category:                    
                 web = txts.find_all('a', href=True)
                 site = web[1].get('href')
-                newlinks.append(site)                    
+                newlinks.append(site)    
+                f.write(str(site) + "\n")                
                 count+=1             
         else:
             category = soup.find_all(class_="jobsearch-result") 
             for txts in category:                    
                 web = txts.find_all('a', href=True)
                 site = web[0].get('href')
-                newlinks.append(site)                    
+                newlinks.append(site)  
+                f.write(str(site) + "\n")                  
                 count+=1 
     print("Newest link extracted")
 
